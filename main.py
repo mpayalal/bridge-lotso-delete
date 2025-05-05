f483e5ca2f15110f3b448f2bebf8e85b20a1e40f
mpayalal <mpayalal@eafit.edu.co>
2025-05-04 23:02:18 -0500

    Se agrega enviar documento a correo

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import os
 import json
-from pydantic import BaseModel
 from aio_pika import connect_robust, Message
 from fastapi import FastAPI, HTTPException, Form
 
 app = FastAPI()
 
 rabbitmq_user = os.getenv("RABBITMQ_USER") 
-print(rabbitmq_user)
 rabbitmq_pass = os.getenv("RABBITMQ_PASSWORD")
-print(rabbitmq_pass)
 rabbitmq_host = os.getenv("RABBITMQ_HOST")
-print(rabbitmq_host)
 rabbitmq_port = os.getenv("RABBITMQ_PORT")
-print(rabbitmq_port)
 
 async def connect_to_rabbit():
     try:
@@ -27,18 +22,18 @@ async def connect_to_rabbit():
     except Exception as e:
         raise Exception(f"RabbitMQ connection error: {e}")
     
-async def publish_to_rabbitmq(message_body: dict):
+async def publish_to_rabbitmq(queue: str, message_body):
     try:
         connection = await connect_to_rabbit()
         async with connection:
             channel = await connection.channel()
-            queue = await channel.declare_queue('delete_file', durable=True)
-            message =  Message(body=message_body.encode())
+            queue = await channel.declare_queue(queue, durable=True)
+            message =  Message(body=json.dumps(message_body).encode())
             await channel.default_exchange.publish(message, routing_key=queue.name)
     except Exception as e:
         raise Exception(f"RabbitMQ publish error: {e}")
 
-@app.post("/v1/documents/deleteFile")
+@app.post("/v1/events/documents/deleteFile")
 async def delete_file_from_bucket(
     client_id: str = Form(...),
     file_name: str = Form(...)
@@ -48,9 +43,30 @@ async def delete_file_from_bucket(
         message = f"{client_id}/{file_name}"
 
         # Mandamos mensaje
-        await publish_to_rabbitmq(message)
+        await publish_to_rabbitmq('delete_file', message)
 
         return {"message": f"El archivo '{file_name}' del cliente {client_id} está siendo procesado para eliminación."}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
+@app.post("/v1/events/documents/sendFile")
+async def send_document_to_email(
+    client_id: str = Form(...),
+    file_name: str = Form(...),
+    to_email: str = Form(...)
+):
+    try:
+        # Mensaje para RabbitMQ
+        message = {
+            "to_email": to_email,
+            "file_name": f"{client_id}/{file_name}"
+        }
+
+        # Mandamos mensaje
+        await publish_to_rabbitmq('notifications', message)
+
+        return {"message": f"El archivo '{file_name}' está siendo procesado para enviarlo al correo {to_email}."}
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
